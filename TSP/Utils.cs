f057ec6ba269e04a5587452f70fd07961f57a1ae
Christian-Hoeller <ch.hoeller14@gmail.com>
2019-12-13 09:51:42 +0100

    Added DrawTour

---
+++ TSP/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public static class Utils
+    {
+        public static double GetDistance(IReadOnlyList<int> solution, IReadOnlyList<Location> cities)
+        {
+            double distance = 0;
+            for (int i = 0; i < solution.Count; i++)
+            {
+                distance += GetDistanceBetweenTwoPoints(cities[solution[i] -1], cities[solution[i]]);
+            }
+            return distance;
+        }
+
+
+        private static double GetDistanceBetweenTwoPoints(Location point1, Location point2)
+        {
+            
+            var lengthX = Math.Abs(point1.X - point2.X);
+            var lengthY = Math.Abs(point1.Y - point2.Y);
+
+            double distance = (Math.Pow(lengthX, 2) + Math.Pow(lengthY, 2));
+
+            return distance;
+
+        }
+    }
+}

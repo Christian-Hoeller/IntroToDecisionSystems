419758f4d1c65541d7f7031b957aec8c6b05c35f
Christian-Hoeller <ch.hoeller14@gmail.com>
2020-01-17 11:08:25 +0100

    Added class MyAwesomeTSPSolver

--- TSP/DummyTSPSolver.cs
+++ TSP/DummyTSPSolver.cs
@@ -10,4 +10,15 @@ namespace TSPSolver.TSP
             return Enumerable.Range(1, cities.Count).ToList();
         }
     }
+
+    public class MyAwesomeTSPSolver : ITSPSolver
+    {
+        public List<int> Solve(IReadOnlyList<Location> cities)
+        {
+            //Eigenen Solver schreiben
+            //algorithums finden um die beste Route herauszufinden
+            //bis 07.02.2019
+            throw new System.NotImplementedException("Homework");
+        }
+    }
 }

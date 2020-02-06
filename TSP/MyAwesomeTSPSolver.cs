26475f5b57933582960daf4a66945c5fbd9cfdf6
Christian-Hoeller <ch.hoeller14@gmail.com>
2020-02-06 17:58:50 +0100

    major changes to the algorithm

--- TSP/MyAwesomeTSPSolver.cs
+++ TSP/MyAwesomeTSPSolver.cs
@@ -8,58 +8,40 @@ namespace TSPSolver.TSP
     {
         public List<int> Solve(IReadOnlyList<Location> cities)
         {
-            //Eigenen Solver schreiben
-            //algorithums finden um die beste Route herauszufinden
-            //bis 07.02.2019
-
             List<int> visitedCities = new List<int>();
-            visitedCities.Add(0);
+            visitedCities.Add(0);   //add the first index of the cities as the startindex
 
-            for (int i = 0; i < cities.Count; i++)
-            {
-                int nearestIndex = NearestNeighbour(cities, visitedCities);
-                visitedCities.Add(nearestIndex);    
-            }
-
-            for (int i = 0; i < visitedCities.Count; i++)
-            {
-                Console.WriteLine("göd: " + visitedCities[i]);
-            }
-
-
-            return visitedCities.Select(i => i + 1).ToList();
+            return RecursiveSolve(cities, visitedCities).Select(i => i + 1).ToList();   //returns the list of indexes and adds +1 to every index
         }
 
-        private int NearestNeighbour(IReadOnlyList<Location> cities, List<int> visitedCities)
+        public List<int> RecursiveSolve(IReadOnlyList<Location> cities, List<int> visitedCities)
         {
-            //visitedCities[visitedCities.Count - 1]
-            Location currentCity = cities[visitedCities[visitedCities.Count - 1]];
-
-            List<double> allDistances = new List<double>();
-            int lowestIndex = 0;
-
-            for (int i = 0; i < cities.Count; i++)
+            if (visitedCities.Count == cities.Count)    //returns the visited Cities if all the cities are visited
             {
-                if(visitedCities.Contains(i) == false)
-                {
-                    var distance = CityDistance(currentCity, cities[i]);
-
-
-                    if (allDistances.Count != 0)
-                    {
-                        if (distance < allDistances.Min())
-                        {
-                            lowestIndex = i;
-                        }
-                    }
-                    else
-                        lowestIndex = i;
-
-                    allDistances.Add(distance);
-                }
+                return visitedCities;
             }
+            else
+            {
+                Location currentCity = cities[visitedCities.Last()];    //gets the currentCity with the index of the last item in visitedCities
+                List<double> allDistances = new List<double>(); //new list where we store all the distances
+                List<int> CitiesNotVisited = GetCitiesNotVisited(cities, visitedCities);    //Gets all the cities that haven't been visited yet
 
-            return lowestIndex;
+                foreach (var city in CitiesNotVisited)
+                {
+                    allDistances.Add(CityDistance(currentCity, cities[city]));  //adds the distances between the currentCity and every other (notVisited)city to the List
+                }
+
+                int lowestIndex = CitiesNotVisited[allDistances.IndexOf(allDistances.Min())];   //gets the index of the lowest distance --> this is the index in citiesNotVisited
+                visitedCities.Add(lowestIndex);     //adds the lowest index to the visitedCities
+                return RecursiveSolve(cities, visitedCities);
+
+            }
+        }
+
+        private List<int> GetCitiesNotVisited(IReadOnlyList<Location> cities, List<int> visitedCities)
+        {
+            List<int> allCityIndexes = Enumerable.Range(0, cities.Count).ToList();     //gets every index in the list
+            return allCityIndexes.Except(visitedCities).ToList();   //excepts the visitedCites from all the city indexes
         }
 
         public static double CityDistance(Location l1, Location l2)
